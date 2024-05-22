c5802bb41d9ce3017d0b1ce6d837641e4a7b6570
Gil Wallach <gil.wallach@gmail.com>
2024-05-22 15:37:38 +0300

    webpack.config updated

--- webpack.config copy.js
+++ webpack.config copy.js
@@ -3,17 +3,10 @@ const path = require("path");
 module.exports = {
   mode: "development",
   entry: "./src/app.ts",
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "dist"), // Serve from 'dist' directory
-    },
-    compress: true, // Enable gzip compression
-    port: 9000,     // Choose a port for the dev server
-  },
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
-    publicPath: "/",
+    publicPath: "/Task-Manager-Bundle/dist/",
   },
   module: {
     rules: [
