b1f61f311b260b59181815114402211a24251ee0
Gil Wallach <gil.wallach@gmail.com>
2024-06-05 20:16:36 +0300

    webpack.config changed back

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,8 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "production",
-  // mode: "development",
+  // mode: "production",
+  mode: "development",
   entry: "./src/app.ts",
   output: {
     filename: "bundle.js",
