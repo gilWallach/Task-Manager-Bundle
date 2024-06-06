e256ade694d25191c6d2d798b30392a9fcde960b
Gil Wallach <gil.wallach@gmail.com>
2024-06-06 11:49:23 +0300

    Test app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import { TaskInput } from "./components/task-input"
 import { TaskList } from "./components/task-list"
-import"./main.css"
+import "./main.css"
 
 console.log('app.ts Working')
-    // INSTANTIATION
-    new TaskInput()
-    new TaskList(`active`)
-    new TaskList(`done`)
+// INSTANTIATION
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('DOM Loaded')
+    new TaskInput();
+    new TaskList('active');
+    new TaskList('done');
+});
