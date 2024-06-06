d204fa81fd74999be01135495c1a132b353f9b13
Gil Wallach <gil.wallach@gmail.com>
2024-06-06 11:40:35 +0300

    Test app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import { TaskInput } from "./components/task-input"
 import { TaskList } from "./components/task-list"
 import"./main.css"
+
+console.log('app.ts Working')
     // INSTANTIATION
     new TaskInput()
     new TaskList(`active`)
