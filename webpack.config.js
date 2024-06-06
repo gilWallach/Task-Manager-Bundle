68a5a7c2ae4a750096caa31e07c65c1e59c6cbc6
Gil Wallach <gil.wallach@gmail.com>
2024-06-06 11:22:03 +0300

    imports adjusted in indes.html & webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,11 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "production",
-  // mode: "development",
   entry: "./src/app.ts",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
-    publicPath: "/Task-Manager-Bundle/",
-    // publicPath: "/", // Adjust to your repository name
+    publicPath: "/Task-Manager-Bundle/", // Adjust to match your repository name
   },
   module: {
     rules: [
@@ -31,6 +29,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
+      inject: 'head', // Ensures the script tag is placed at the end of the body
     }),
   ],
   devtool: "inline-source-map",
